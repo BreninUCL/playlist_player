af6b5a51014bf4a857dd219fce42e01061cc7dc8
Miguel Estevam <miguelfrancaestevamlopes@gmail.com>
2024-11-18 20:09:49 -0300

    Concertando a cagada

--- src/components/DeviceSelector.jsx
+++ src/components/DeviceSelector.jsx
@@ -9,12 +9,10 @@ const DeviceSelector = () => {
   const [error, setError] = useState('');
   const [selectedDeviceId, setSelectedDeviceId] = useState(null);
   const [playlistContent, setPlaylistContent] = useState(null);
-  const [cardapioContent, setCardapioContent] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
-  const [isDeviceChangeAllowed, setIsDeviceChangeAllowed] = useState(true);
   const intervalRef = React.useRef();
 
   const fetchDevices = async () => {
@@ -26,7 +24,7 @@ const DeviceSelector = () => {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
+          'Authorization': `Bearer ${token}`,
         },
       });
 
@@ -39,92 +37,9 @@ const DeviceSelector = () => {
       setDevices(data.data);
     } catch (err) {
       setError(err.message);
-      console.error('Erro ao buscar dispositivos:', err);
+      console.error(err);
     }
   };
-
-const cacheCardapio = async (cardapio) => {
-  const cache = await caches.open(CACHE_NAME);
-
-  try {
-    // Itera sobre o conteúdo do cardápio para identificar mídias a serem cacheadas
-    const mediaPromises = cardapio.content.map(async (categoria) => {
-      if (categoria.produtos && Array.isArray(categoria.produtos)) {
-        for (const produto of categoria.produtos) {
-          if (produto.media && produto.media.file_url) {
-            const mediaUrl = `${cardapio.base_url}/${produto.media.file_url}`;
-            try {
-              const cachedResponse = await cache.match(mediaUrl);
-
-              if (!cachedResponse) {
-                const response = await fetch(mediaUrl);
-                if (!response.ok) {
-                  throw new Error('Erro no download da mídia ' + mediaUrl);
-                }
-                await cache.put(mediaUrl, response.clone());
-              }
-            } catch (err) {
-              console.error('Erro ao cachear mídia do cardápio:', err);
-              produto.media.error = 'Falha ao cachear a mídia';
-            }
-          }
-        }
-      }
-    });
-
-    await Promise.all(mediaPromises);
-  } catch (err) {
-    console.error('Erro ao cachear mídias do cardápio:', err);
-    throw new Error('Falha ao cachear todas as mídias do cardápio.');
-  }
-};
-
-// Modifique a função fetchCardapio para incluir o cacheamento após buscar o cardápio com sucesso
-const fetchCardapio = async (cardapioId) => {
-  try {
-    console.log('Buscando cardápio com ID:', cardapioId);
-    const response = await fetch(`${API_URL}/cardapio/${cardapioId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      },
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.message || 'Erro ao buscar cardápio');
-    }
-
-    const data = await response.json();
-    console.log('Cardápio recebido:', data);
-
-    if (data?.data?.length > 0) {
-      const cardapio = data.data[0];
-      if (cardapio?.content) {
-        try {
-          cardapio.content = JSON.parse(cardapio.content);
-          console.log('Conteúdo do cardápio convertido:', cardapio.content);
-
-          // Adiciona cacheamento aqui
-          await cacheCardapio(cardapio);
-        } catch (err) {
-          console.error('Erro ao parsear conteúdo do cardápio:', err);
-          setError('Erro ao processar o conteúdo do cardápio');
-          return;
-        }
-      }
-      setCardapioContent(cardapio);
-    } else {
-      setError('O cardápio está vazio.');
-      console.warn('O cardápio retornou vazio:', data);
-    }
-  } catch (err) {
-    setError(err.message);
-    console.error('Erro ao buscar cardápio:', err);
-  }
-};
-
 
   const handleDeviceSelection = async (e) => {
     e.preventDefault();
@@ -134,111 +49,85 @@ const fetchCardapio = async (cardapioId) => {
       return;
     }
 
-    const selectedDevice = devices.find(
-      (device) => device.id === parseInt(selectedDeviceId)
-    );
+    const selectedDevice = devices.find(device => device.id === parseInt(selectedDeviceId));
 
-    if (!selectedDevice.playlist_id && !selectedDevice.cardapio_id) {
-      setError('Este dispositivo não tem playlist ou cardápio associado.');
+    if (!selectedDevice.playlist_id) {
+      setError('Este dispositivo não tem uma playlist associada.');
       return;
     }
 
     setIsLoading(true);
     enterFullScreen();
-    setIsDeviceChangeAllowed(false);
 
     try {
-      if (selectedDevice.playlist_id) {
-        const playlistResponse = await fetch(
-          `${API_URL}/playlist/${selectedDevice.playlist_id}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-            },
-          }
-        );
+      const response = await fetch(`${API_URL}/playlist/${selectedDevice.playlist_id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+        },
+      });
 
-        if (!playlistResponse.ok) {
-          const errorData = await playlistResponse.json();
-          throw new Error(errorData.message || 'Erro ao buscar playlist');
-        }
-
-        const playlistData = await playlistResponse.json();
-        await cachePlaylistMedia(playlistData);
-
-        setPlaylistContent(playlistData);
-        setCurrentIndex(0);
-        setIsPlaying(true);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erro ao buscar playlist');
       }
 
-      if (selectedDevice.cardapio_id) {
-        await fetchCardapio(selectedDevice.cardapio_id);
-      }
+      const data = await response.json();
+      
+      await cachePlaylistMedia(data);
 
-      if (selectedDevice.playlist_id || selectedDevice.cardapio_id) {
-        setIsPlaying(true);
-      }
-
+      setPlaylistContent(data);
+      setCurrentIndex(0);
+      setIsPlaying(true);
       setIsLoading(false);
     } catch (err) {
       setError(err.message);
-      console.error('Erro na seleção do dispositivo:', err);
+      console.error(err);
       setIsLoading(false);
     }
   };
 
-  const handleDeviceChange = () => {
-    setIsDeviceChangeAllowed(true);
-    setIsPlaying(false);
-    setPlaylistContent(null);
-    setCardapioContent(null);
-    clearInterval(intervalRef.current);
-  };
-
   const cachePlaylistMedia = async (playlist) => {
     const cache = await caches.open(CACHE_NAME);
-
+  
     try {
       const mediaPromises = playlist.contents.map(async (item) => {
         if (item.contentType === 'file' && item.media) {
           const mediaUrl = `${playlist.base_url}/${item.media.file_url}`;
-
+  
           try {
             const cachedResponse = await cache.match(mediaUrl);
-
+  
             if (cachedResponse) {
               const blob = await cachedResponse.blob();
               item.media.cachedUrl = URL.createObjectURL(blob);
             } else {
               const response = await fetch(mediaUrl);
-
+  
               if (!response.ok) {
                 throw new Error('Erro no Download do Arquivo ' + mediaUrl);
               }
-
+  
               const responseClone = response.clone();
               const blob = await response.blob();
-
+  
               await cache.put(mediaUrl, responseClone);
-
+  
               item.media.cachedUrl = URL.createObjectURL(blob);
             }
           } catch (err) {
             console.error('Erro ao baixar mídia:', err);
             item.media.error = 'Falha ao baixar a mídia';
-            throw err;
+            throw err; // Lançando o erro para interromper o Promise.all
           }
         }
       });
-
-      await Promise.all(mediaPromises);
+  
+      await Promise.all(mediaPromises); //promise.all que o professor falou
     } catch (err) {
       console.error('Erro ao cachear todas as mídias:', err);
-      throw new Error(
-        'Falha ao cachear todas as mídias, o carregamento foi interrompido.'
-      );
+      throw new Error('Falha ao cachear todas as mídias, o carregamento foi interrompido.');
     }
   };
 
@@ -270,80 +159,25 @@ const fetchCardapio = async (cardapioId) => {
   }, [isPlaying, playlistContent, currentIndex]);
 
   const enterFullScreen = () => {
-    const element = document.querySelector('.device-selector'); // Coloca o contêiner no modo tela cheia
+    const element = document.documentElement;
     if (element.requestFullscreen) {
       element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) { /* Safari */
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
       element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { /* IE11 */
+    } else if (element.msRequestFullscreen) {
       element.msRequestFullscreen();
     }
   };
 
-  const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
-
-  useEffect(() => {
-    if (cardapioContent?.content?.length > 2) {
-      const interval = setInterval(() => {
-        setCurrentCategoryIndex((prevIndex) => 
-          (prevIndex + 2) % cardapioContent.content.length
-        );
-      }, 5000); // Troca a cada 5 segundos, ajuste conforme necessário
-  
-      return () => clearInterval(interval); // Limpa o intervalo ao desmontar
-    }
-  }, [cardapioContent]);
-  
-  const renderCardapio = () => {
-    if (!cardapioContent || !cardapioContent.content) {
-      return <p>O cardápio está vazio.</p>;
-    }
-  
-    // Filtra apenas duas categorias por vez
-    const categoriasExibidas = cardapioContent.content.slice(
-      currentCategoryIndex, 
-      currentCategoryIndex + 2
-    );
-  
-    return (
-      <div className={`cardapio ${isFullScreen ? 'fullscreen' : ''}`}>
-        <h3 className="cardapio-title">{cardapioContent.name}</h3>
-        <p className="cardapio-description">{cardapioContent.description}</p>
-        <div className="cardapio-categorias">
-          {categoriasExibidas.map((categoria, index) => (
-            <div key={index} className="categoria">
-              <h4 className="categoria-title">{categoria.nome_categoria}</h4>
-              <ul className="categoria-list">
-                {categoria.produtos.map((produto, idx) => (
-                  <li key={idx} className="produto-item">
-                    <div className="produto-name">
-                      <strong>{produto.nome}</strong>: R$ {produto.preco}
-                    </div>
-                    {produto.descricao && (
-                      <p className="produto-description">{produto.descricao}</p>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
-  
-  
-
   const renderCurrentContent = () => {
-    if (cardapioContent) {
-      return renderCardapio();
-    }
+    const currentItem = playlistContent.contents[currentIndex];
 
-    if (!playlistContent || !playlistContent.contents.length) {
+    if (!currentItem) {
       return <p>Nenhum conteúdo disponível.</p>;
     }
 
-    const currentItem = playlistContent.contents[currentIndex];
     const { base_url } = playlistContent;
     const mediaUrl = `${base_url}/${currentItem.media?.file_url}`;
     const cachedUrl = currentItem.media?.cachedUrl || mediaUrl;
@@ -351,7 +185,11 @@ const fetchCardapio = async (cardapioId) => {
     if (currentItem.contentType === 'file' && currentItem.media) {
       if (currentItem.media.file_extension === 'mp4') {
         return (
-          <video autoPlay loop className="media-fullscreen">
+          <video
+            autoPlay
+            loop
+            className="media-fullscreen"
+          >
             <source src={cachedUrl} type="video/mp4" />
             Seu navegador não suporta a tag de vídeo.
           </video>
@@ -367,44 +205,61 @@ const fetchCardapio = async (cardapioId) => {
       }
     } else if (currentItem.contentType === 'html' && currentItem.html) {
       return (
-        <div
-          className="html-content"
-          dangerouslySetInnerHTML={{ __html: currentItem.html.content }}
-        />
+        <div className="html-content" dangerouslySetInnerHTML={{ __html: currentItem.html.content }} />
       );
     }
   };
 
+  const LoadingModal = () => (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <p>Carregando mídias, por favor aguarde...</p>
+      </div>
+    </div>
+  );
+
+  const ExitModal = () => (
+    <div className="exit-modal">
+      <button onClick={() => {
+        setIsPlaying(false);
+      }} className="exit-button">
+        Alterar Dispositivo
+      </button>
+    </div>
+  );
+
   return (
-    <div className={`device-selector ${isFullScreen ? 'fullscreen-mode' : ''}`}>
+    <div className="device-selector-container">
+      <h2 className="title">Selecionar Dispositivo</h2>
       {error && <p className="error">{error}</p>}
-      {isDeviceChangeAllowed ? (
-        <form onSubmit={handleDeviceSelection}>
-          <label htmlFor="device-select">Selecione um dispositivo:</label>
-          <select
-            id="device-select"
-            value={selectedDeviceId || ''}
-            onChange={(e) => setSelectedDeviceId(e.target.value)}
-          >
-            <option value="">-- Selecione um dispositivo --</option>
-            {devices.map((device) => (
-              <option key={device.id} value={device.id}>
-                {device.name}
-              </option>
-            ))}
-          </select>
-          <button type="submit" disabled={isLoading}>
-            {isLoading ? 'Carregando...' : 'Selecionar'}
-          </button>
-        </form>
-      ) : (
-        <div>
-          {!isFullScreen && (
-            <button onClick={handleDeviceChange}>Alterar dispositivo</button>
-          )}
+      <form onSubmit={handleDeviceSelection} className="device-selector-form">
+        <select
+          value={selectedDeviceId || ''}
+          onChange={(e) => {
+            localStorage.setItem('selected_device', e.target.value);
+            setSelectedDeviceId(e.target.value);
+          }}
+          className="device-select"
+        >
+          <option value="">Selecione um dispositivo</option>
+          {devices.map((device) => (
+            <option key={device.id} value={device.id}>
+              {device.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit" className="submit-button">Carregar Playlist</button>
+      </form>
+
+      {isPlaying && playlistContent && (
+        <div className="playlist-content">
           {renderCurrentContent()}
         </div>
       )}
+
+      {isLoading && <LoadingModal />}
+
+      {isPlaying && !isFullScreen && <ExitModal />}
     </div>
   );
 };
